1262f767557f6ab3ee8eabc71fc3f7bedb6f7866
MistaBaaz <88873271+mistabaaz@users.noreply.github.com>
2023-05-27 17:01:09 +0530

    Solution for exercise 1

---
+++ ex1_calculator.py
@@ -0,0 +1,12 @@
+# A simple calculator using python
+num1 = int(input("enter first number :"))
+num2 = int(input("enter second number : " ))
+add = num1+num2
+print("addition of given two numbers is ",add)
+diff = num1-num2
+print("diffrence of given two numbers is ",diff)
+mult = num1*num2
+print("by multiplying given two number we get ",mult)
+div = num1 / num2
+print("by dividing the given two number we get ",div)
+#end of the program
