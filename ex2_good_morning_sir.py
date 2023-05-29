8a124e6f67da6d789ec638a5d7997428169ee552
MistaBaaz <88873271+mistabaaz@users.noreply.github.com>
2023-05-29 16:59:28 +0530

    Solution for exercise 2

---
+++ ex2_good_morning_sir.py
@@ -0,0 +1,15 @@
+# A program to greet user according to time
+
+import time
+t = time.localtime()  # takes the time from current machine
+hours = t.tm_hour   # provide the value of current hour
+if hours<=12:
+    print("Good Morning Sir!")
+elif hours<=16:
+    print("Good Afternoon Sir!")
+elif hours<=20:
+    print("Good Evening Sir!")
+else:
+    print("Good Night Sir!")
+
+#Finished
