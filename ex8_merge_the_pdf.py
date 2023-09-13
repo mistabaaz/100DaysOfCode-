06798c20cf137273d4c987911007165979bbbb13
MistaBaaz <88873271+mistabaaz@users.noreply.github.com>
2023-09-13 21:28:44 +0530

    Solution for exercise 8

---
+++ ex8_merge_the_pdf.py
@@ -0,0 +1,155 @@
+import pypdf
+import os
+
+
+def ck_int():  #to conquer the error during input
+    while True:
+        try:
+            var = input("Choose from the above option: ")
+            var = int(var)
+            break
+        except ValueError:
+            print("Please enter appropiate option!")
+
+    return var
+
+
+def fetch_pdf(path):   # to fetch all pdf files 
+    pdf = []
+    files = sorted(os.listdir(path))
+    for file in files:
+        fp = f"{path}\\{file}"  #file path
+        ext = os.path.splitext(fp)[1].lower() #extension of file
+        #condition for pdf file
+        if os.path.isfile(fp) and ext == ".pdf":
+            pdf.append(file)
+
+    return pdf
+            
+def merged_pdf(path):
+    merger = pypdf.PdfWriter()
+    pdf = fetch_pdf(path)
+    for file in pdf:
+        merger.append(file)
+    merger.write(f"{path}/merged.pdf")
+    merger.close()
+    return f"{path}\\merged.pdf"
+
+def encrypt_pdf(path,pwd):
+    reader = pypdf.PdfReader(path)
+    writer = pypdf.PdfWriter()
+    for page in reader.pages:
+        writer.add_page(page)
+    writer.encrypt(pwd)
+    with open("encrypted.pdf","wb") as file:
+        writer.write(file)    
+    return f"{path}\\encrypted.pdf"
+
+def decrypt_pdf(path,pwd):
+    reader = pypdf.PdfReader(path)
+    writer = pypdf.PdfWriter()
+    if reader.is_encrypted:
+        reader.decrypt(pwd)
+    for page in reader.pages:
+        writer.add_page(page)
+    with open("decrypted.pdf","wb") as file:
+        writer.write(file)
+    return f"{path}\\decrypted.pdf"
+    
+
+if __name__ == "__main__":
+
+    print("Welcome to PDFtools".center(50, "*"), "\n")
+    cdir = os.getcwd()  #current dir
+    print("If you want to use current path then press enter")
+    print(f"Current working path is {cdir}")
+    path = input("Enter the path: ")
+    if not (path):
+        path = cdir  #current path
+
+    while True:
+        #Main Menu
+        print("HomePage".center(25,"="))
+        print("\n1.PDF files in current folder")
+        print("2.Merge the all PDFs present in current folder")
+        print("3.Custom Merging")
+        print("4.Encrypt the PDF")
+        print("5.Decrypt the PDF")
+        print("6.Change the current folder")
+        print("7.Exit\n")
+        choice = ck_int()
+
+        if (choice == 1):
+
+            pdf = fetch_pdf(path)
+            if not pdf:
+                print("\n No pdf is avalible in current folder!!")
+            else :
+                print(f"\nAvailible PDF's : {pdf}")
+
+        elif (choice == 2):
+
+            print("\n\nNote: pdf files are merged acc. to alphabet sorting!")
+            print("Therefore you must place your pdf in alphabet sorting.")
+            print(f"\nCurrent files in order are: {fetch_pdf(path)}")
+            ans = input("\n\nDo you want to merge the pdf(y/n): ")
+            if not fetch_pdf(path):
+                print("\nNo pdf files to merge!!")
+                continue
+            if ans.lower() == "y":
+                out = merged_pdf(path)
+                print(f"\nHere is your merged pdf stored: \n{out}")
+
+        elif (choice == 3):
+
+            print("\nAvailible Soon!!!")
+
+        elif (choice == 4):
+
+            print("Encryption".center(25,"="))
+            pdf = fetch_pdf(path)
+            print("\nChoose from the current pdf files: ")
+            print(pdf)
+            print("Please choose from above pdfs(1,2,3...)\n")
+            ch = ck_int() #choice
+            if ch not in range(len(pdf)+1):
+                print("\nplease choose from above opotion!!")
+                continue
+            pwd = input(f"Enter the password for {pdf[ch-1]}: ")
+            out =  encrypt_pdf(f"{path}\\{pdf[ch-1]}",pwd)
+            print(f"\nHere is your encrypted pdf: \n{out}")
+
+        elif (choice == 5):
+
+            print("Decryption".center(25,"="))
+            pdf = fetch_pdf(path)
+            print("\nChoose from the current pdf files: ")
+            print(pdf)
+            print("Please choose from above pdfs(1,2,3...)\n")
+            ch = ck_int() #choice
+            if ch not in range(len(pdf)+1):
+                print("\nplease choose from above opotion!!")
+                continue
+            pwd = input(f"Enter the password for {pdf[ch-1]}: ")
+            out =  encrypt_pdf(f"{path}\\{pdf[ch-1]}",pwd)
+            print(f"\nHere is your decrypted pdf: \n{out}")
+
+        elif (choice == 6):
+
+            folder = input("Enter full path of the folder: ")
+            if not folder:
+                print("Enter Valid Input!!")
+                continue
+            os.chdir(folder)
+            print(f"Now your current directory is {os.getcwd()}")
+
+        elif (choice == 7):
+
+            ans = input("\n\n Confirm to exit (y/n): ")
+            if ans.lower() == "y":
+                break
+            else:
+                print("Please enter valid input!!")
+
+        else:
+            print("\n\n Please choose correct option!!")
